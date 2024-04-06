89ee2678ef9933f9759cb9fb45513e2e70e50995
daryazaitseva <zaytsevaart@gmail.com>
2024-04-06 13:48:08 +0700

    last changes

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 n = int(input('Введите объем последнего кубика: '))
 v = 0
-for i in range(1, 100001):
+for i in range(1, int(n ** (1 / 3)) + 1):
     v = i**3
     if v <= n:
         print(v, end=' ')
